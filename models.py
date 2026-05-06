c7ca23bed3a6b9c1b09f14c8745e1fea4a95f341
Vincent König <vincentkoenig97@gmail.com>
2026-05-06 11:43:27 +0200

    Add database models for User and Movie

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
