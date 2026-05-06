d04e8fe4f152e49417389396b4a34e4f0082c9b2
Vincent König <vincentkoenig97@gmail.com>
2026-05-06 15:05:49 +0200

    Add all routes for user and movie management

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from data_manager import DataManager
 from models import db, Movie
 import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
 
 app = Flask(__name__)
 
@@ -14,9 +20,46 @@ db.init_app(app)
 data_manager = DataManager()
 
 @app.route('/')
-def home():
-    return "Welcome to MoviWeb App!"
+def index():
+    users = data_manager.get_users()
+    return render_template('index.html', users=users)
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    name = request.form.get('name')
+    data_manager.create_user(name)
+    return redirect(url_for('index'))
+
+@app.route('/users/<int:user_id>/movies')
+def get_movies(user_id):
+    movies = data_manager.get_movies(user_id)
+    return render_template('movies.html', movies=movies)
+
+@app.route('/users/<int:user_id>/movies', methods=['POST'])
+def add_movie(user_id):
+    title = request.form.get('name')
+    response = requests.get(f"http://www.omdbapi.com/?t={title}&apikey={api_key}")
+    data = response.json()
+    movie = Movie(
+        name=data['Title'],
+        director=data['Director'],
+        year=data['Year'],
+        poster_url=data['Poster'],
+        user_id=user_id
+    )
+    data_manager.add_movie(movie)
+    return redirect(url_for('get_movies', user_id=user_id))
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+def update_movie(user_id, movie_id):
+    title = request.form.get('name')
+    data_manager.update_movie(movie_id, title)
+    return redirect(url_for('get_movies', user_id=user_id))
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    data_manager.delete_movie(movie_id)
+    return redirect(url_for('get_movies', user_id=user_id))
 
 if __name__ == '__main__':
   with app.app_context():
