34a6d5f7734fe40e68e477d43c32c0e939cfed57
Vincent König <vincentkoenig97@gmail.com>
2026-05-06 12:05:34 +0200

    Updated names

--- data_manager.py
+++ data_manager.py
@@ -2,29 +2,29 @@ from models import db, User, Movie
 
 class DataManager():
     def create_user(self, name):
-        new_user = User(name=name)
-        db.session.add(new_user)
+        user = User(name=name)
+        db.session.add(user)
         db.session.commit()
 
     def get_users(self):
-        all_users = User.query.all()
-        return all_users
+        users = User.query.all()
+        return users
 
     def get_movies(self, user_id):
-        all_movies = Movie.query.filter_by(user_id=user_id).all()
-        return all_movies
+        movies = Movie.query.filter_by(user_id=user_id).all()
+        return movies
 
     def add_movie(self, movie):
         db.session.add(movie)
         db.session.commit()
 
     def update_movie(self, movie_id, new_title):
-        updated_movie = Movie.query.get(movie_id)
-        updated_movie.name = new_title
+        movie = Movie.query.get(movie_id)
+        movie.name = new_title
         db.session.commit()
 
 
     def delete_movie(self, movie_id):
-        deleted_movie = Movie.query.get(movie_id)
-        db.session.delete(deleted_movie)
+        movie = Movie.query.get(movie_id)
+        db.session.delete(movie)
         db.session.commit()
